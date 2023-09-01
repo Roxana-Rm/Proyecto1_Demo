0d9ab96deecea64240248ca8e62a0eef3b78f160
Computo 5 Zacatecoluca <zc301@itca-fepade.edu.sv>
2023-09-01 11:05:20 -0600

    correccion de la lista de nombre

--- lib/pages/myhome.dart
+++ lib/pages/myhome.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-List<String> nombre = String ["Luis","Juan","Mario","Daniel","Oscar"];
+List<String> nombre = <String> ["Luis","Juan","Mario","Daniel","Oscar"];
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
